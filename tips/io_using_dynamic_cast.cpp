64d162df9fd575915f6cb6e2d8122621f5c4face
Aleksey Karpov <kiwhyd@gmail.com>
2021-08-28 18:22:54 +0300

    Create io_using_dynamic_cast.cpp

---
+++ tips/io_using_dynamic_cast.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <new>
+
+void handler()
+{
+    std::cout << "Memory allocation failed, terminating" << std::endl;
+    std::set_new_handler(nullptr);
+}
+
+int main()
+{
+    std::set_new_handler(handler);
+
+    try 
+    {
+        while (true) 
+        {
+            new int[100000000ul];
+        }
+    }
+    catch (const std::bad_alloc& e) 
+    {
+        std::cout << e.what() << '\n';
+    }
+}
